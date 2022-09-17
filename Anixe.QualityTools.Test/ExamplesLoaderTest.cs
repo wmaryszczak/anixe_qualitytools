d6fc13feb45c86d5e5446676ac499d73ab734d40
lechu445 <xxx@example.com>
2022-09-17 17:43:35 +0200

    Update dependencies
    Also changed exception type in LoadProjectRootPath() and added more info to csproj

--- Anixe.QualityTools.Test/ExamplesLoaderTest.cs
+++ Anixe.QualityTools.Test/ExamplesLoaderTest.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace Anixe.QualityTools.Test
@@ -8,15 +6,6 @@ namespace Anixe.QualityTools.Test
   {
     [Fact]
     public void Should_Load_Examples()
-    {
-      Assert.Equal("{\"name\": \"test\"}", TestExample.ReadAllText(this.GetType(), "json"));
-      Assert.Equal("<test>ExamplesLoaderTest<test>", TestExample.ReadAllText(this.GetType()));
-      Assert.Equal("<test>Should_Load_Examples<test>", TestExample.LoadTestFixture(this.GetType()));
-      Assert.Equal("<test>Should_Load_Examples<test>", TestExample.LoadTestFixture());
-    }
-
-    [Fact]
-    public async Task Should_Load_ExamplesAsync()
     {
       Assert.Equal("{\"name\": \"test\"}", TestExample.ReadAllText(this.GetType(), "json"));
       Assert.Equal("<test>ExamplesLoaderTest<test>", TestExample.ReadAllText(this.GetType()));
